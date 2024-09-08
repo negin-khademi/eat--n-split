ab5c0e2f374c756a5b8eb399fbdf7fa19b8fb8dc
Negin <negin.khademi@gmail.com>
2024-09-08 13:38:43 +0330

    building the static app forms

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -35,7 +38,7 @@ function FriendsList() {
     <div className="sidebar">
       <ul>
         {friends.map((friend) => (
-          <Friend friend={friend} />
+          <Friend key={friend.id} friend={friend} />
         ))}
       </ul>
     </div>
@@ -58,7 +61,49 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👭Friend name</label>
+      <input type="text" />
+
+      <label>🌆Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Spilt a bil with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍‍♀️ Your expense</label>
+      <input type="text" />
+
+      <label>👭 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split a bill</Button>
+    </form>
   );
 }
